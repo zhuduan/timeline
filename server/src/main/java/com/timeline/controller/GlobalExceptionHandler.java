95eff863174f87db5d019e643c64bcd40093346e
Granado <424237052@qq.com>
2017-11-29 23:50:12 +0800

    修改

--- server/src/main/java/com/timeline/controller/GlobalExceptionHandler.java
+++ server/src/main/java/com/timeline/controller/GlobalExceptionHandler.java
@@ -18,7 +18,7 @@ public class GlobalExceptionHandler {
 	public Object exceptionHandler(HttpServletRequest request,
 								   HttpServletResponse response,
 								   Exception exception) throws Exception {
-		
+
 		response.setStatus(200);
 		WebUtils.clearErrorRequestAttributes(request);
 
