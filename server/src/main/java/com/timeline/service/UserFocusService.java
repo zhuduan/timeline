334f8d8c8c5df6cf72e7a5c10766b75751faac0c
Haifeng Zhu <Haifeng.Zhu@activenetwork.com>
2018-01-09 17:18:05 +0800

    add focus api and related services ( not test completed)

--- server/src/main/java/com/timeline/service/UserFocusService.java
+++ server/src/main/java/com/timeline/service/UserFocusService.java
@@ -7,5 +7,10 @@ import java.util.List;
 public interface UserFocusService {
 
     List<UserFocusDTO> getFocusListByUID(Integer UID, Integer pageNum, Integer pageSize);
-
+    
+    Boolean focusSubject(Integer userID, Integer subjectID);
+    
+    Boolean unFocusSubject(Integer userID, Integer subjectID);
+    
+    Boolean isFocusSubject(Integer userID, Integer subjectID);
 }
--- server/src/main/java/com/timeline/service/impl/UserFocusServiceImpl.java
+++ server/src/main/java/com/timeline/service/impl/UserFocusServiceImpl.java
@@ -1,11 +1,13 @@
 package com.timeline.service.impl;
 
 import com.github.pagehelper.PageHelper;
+import com.timeline.common.CommonConfig;
 import com.timeline.model.DTO.UserFocusDTO;
 import com.timeline.model.PO.UserFocus;
 import com.timeline.repository.UserFocusDAO;
 import com.timeline.service.UserFocusService;
 import com.timeline.util.ConvertUtils;
+import com.timeline.util.NumberUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +22,23 @@ public class UserFocusServiceImpl implements UserFocusService {
     @Override
     public List<UserFocusDTO> getFocusListByUID(Integer userID, Integer pageNum, Integer pageSize) {
         PageHelper.startPage(pageNum, pageSize);
-        List<UserFocus> focusList = userFocusDAO.getFocusListByUID(userID);
+        List<UserFocus> focusList = userFocusDAO.getFocusListByUID(userID, CommonConfig.DATA_VALID);
         return ConvertUtils.convert(focusList, UserFocusDTO.class);
+    }
+
+    @Override
+    public Boolean focusSubject(Integer userID, Integer subjectID) {
+        return ( userFocusDAO.focusSubject(userID, subjectID) >= 0 );
+    }
+
+    @Override
+    public Boolean unFocusSubject(Integer userID, Integer subjectID) {
+        return ( userFocusDAO.unFocusSubject(userID, subjectID) >= 0 );
+    }
+
+    @Override
+    public Boolean isFocusSubject(Integer userID, Integer subjectID) {
+        UserFocus userFocus = userFocusDAO.isFocusedSubject(userID, subjectID, CommonConfig.DATA_VALID);
+        return ( userFocus!=null && NumberUtil.isPositiveAndValid(userFocus.getId()));
     }
 }
