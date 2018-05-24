fc37a4209a29810bed57c815d7f25b6ce2d6c24c
zhuduan <zhf8812@gmail.com>
2018-05-24 22:51:57 +0800

    add reply list info

---
+++ web/timeline_web/src/components/replyList.vue
@@ -0,0 +1,17 @@
+<template>
+    <Row>
+        <Col span="12">col-12</Col>
+        <Col span="12">col-12</Col>
+    </Row>
+    <Page :total="100"></Page>
+</template>
+
+<script>
+    export default {
+        name: "replyList"
+    }
+</script>
+
+<style scoped>
+
+</style>
